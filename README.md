78f66ba520fdc78b2f325ae31809da808242ecc3
Pål Hofset Skeide <palhs@Pals-MacBook-Pro.local>
2023-04-29 11:46:51 +0200

    Update readme and bolia script

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Bolianer
-Bolianer is a project for monitoring the available furniture that is on sale on Bolia. They have a lot of bargains throughout the seasons, but it is hard to monitor this by one self. This will be deployed using Databutton or similar where an e-mail notification can be set up for specific products at different nearby locations. Happy furnituring :)
+Bolianer is a project for monitoring the available furniture that is on sale on Bolia. They have a lot of bargains throughout the seasons, but it is hard to monitor this by one self. This project is deployed using Databutton or similar where an e-mail notification can be set up for specific products at different nearby locations.
+
+## Use
+See [Bolianer at Databutton](https://databutton.com/v/9amdy4g2) for deployed modules using Databutton.
+
+## TODO
+This project will soon be deployed using Heroku or similar as a stand alone web app.
+
+Happy furnituring :)
--- scripts/bolia.py
+++ scripts/bolia.py
@@ -13,119 +13,120 @@ try:
 except:
     products = {}
 
+# GET NEW DATA
+
 headers = {
     "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0",
     "Content-Type": "application/json"
     # 'referer': my_referer
 }
 
-# PARAMS
-# condition = perfect, packingok, productok
-# retailType = storepickup, hubpickup, delivery
-# locationName = bgo-214b, bgo-b, boo-229b, 
-# size = 1000 # probably the maximum.
-
-# locationNameDict = {
-#     "Bergen - Vaskerelven" : "bgo-214b",
-#     "Bergen - LMT" : "bgo-b",
-#     "Bodø - City Nord" : "boo-229b",
-# }
-
-
-#What is actually done behind the curtains
-# https://www.bolia.com/api/search/outlet?
-# condition=perfect&
-# includerangelimits=true&
-# language=nb-no&
-# lastfacet=locationName&
-# locationName=osl-217b&
-# mode=category&
-# pageLink=5471&
-# v=2023.5923.0320.1-122
-
-
-
-
 params = {
     # "condition": "perfect",
     # "locationName":"osl-217b",
-    "includerangelimits":"true",
-    "lastfacet":"locationName",
+    # "includerangelimits": "false",
+    "lastfacet": "locationName",
     # "pageLink":"5471",
     # "v":"2023.5923.0320.1-122",
-    "language":"nb-no",
+    "language": "nb-no",
     # "mode":"category"
 }
 
-
-
-# url = "https://www.bolia.com/nb-no/mot-oss/butikker/online-outlet/"
 url = "https://www.bolia.com/api/search/outlet?"
-response = requests.get(url,params=params, headers=headers)
-# response = requests.get("https://www.bolia.com/api/search/outlet?condition=perfect&includerangelimits=true&language=nb-no&lastfacet=locationName&locationName=osl-217b&mode=category&pageLink=5471&v=2023.5923.0320.1-122")
+response = requests.get(url, params=params, headers=headers)
 content = response.text
-page_json = json.loads(content) #To use the json loads, we would need to use the API directly and not just the get from the website itself.
+page_json = json.loads(content)
 nr_of_results = page_json["products"]["total"]
-print(f"Number of results {nr_of_results}.\n")
+# print(f"Number of results {nr_of_results}\n")
 
-# products = {} # This should be a JSON that is easily searchable.
+# Loop through all product categories, category wise since the maximum number of data than can be queries is 1k products... no pages or higher query can be made actually :P
+categories = [cat["value"] for cat in page_json["facets"][5]["options"]]
+# print(f"Category values: {categories}")
 
-for product in page_json["products"]["results"][0]["results"]:
+# print("Plassering:")
+# print(json.dumps(page_json["facets"][2], indent = 3))
 
-    # Check if product is not already in json using the URL path which needs to be unique (at least for this product), if they reuse, then this needs to be reviewed and changed
-    url_id = product["urlPath"]
-    discount_percentage = product["discountPercentage"]
+locations = {}
+for location in page_json["facets"][2]["options"]:
+    locations[location["title"]] = {
+        "ID": location["value"],
+        "count": location["count"],
+    }
+# print(locations)
 
-    update = False
-    if url_id not in products:
-        update = True
-    else:
-        # Check if discount has changed for the same URL, then update URL.
-        if discount_percentage != products[url_id]["discount"]:
+for category in categories:
+    params = {
+        "lastfacet": "room",
+        "language": "nb-no",
+        "room": category,
+        "size": 1000,  # 1000 is the maximum number of products in one query This also applies to their website catually!
+    }
+    time.sleep(1)
+    response = requests.get(url, params=params, headers=headers)
+    # response = requests.get("https://www.bolia.com/api/search/outlet?condition=perfect&includerangelimits=true&language=nb-no&lastfacet=locationName&locationName=osl-217b&mode=category&pageLink=5471&v=2023.5923.0320.1-122")
+    content = response.text
+    page_json = json.loads(content)
+
+    for product in page_json["products"]["results"][0]["results"]:
+        # Check if product is not already in json using the URL path which needs to be unique (at least for this product), if they reuse, then this needs to be reviewed and changed
+        url_id = product["urlPath"]
+        discount_percentage = product["discountPercentage"]["value"]
+
+        update = False
+        if url_id not in products:
             update = True
-
-    if update:
-        # print(json.dumps(product,indent=2))
-
-        # Check location if product is in one of the local stores
-        loc = product.get("location","no location")
-        if loc != "no location":
-            location_ID = product["location"]["inventoryLocationId"]
-            hub = product["location"]["hub"]
-            location_name = product["location"]["name"]
         else:
-            # This susually has to do with the fact that they are shipping the product directly from producer!
-            location_ID = "none"
-            hub = "none"
-            location_name = "none"
+            # Check if discount has changed for the same URL, then update URL.
+            if discount_percentage != products[url_id]["discount"]:
+                update = True
 
-        
+        if update:
+            # print(json.dumps(product,indent=2))
 
-        url_full = "https://www.bolia.com/nb-no/mot-oss/butikker/online-outlet/produkt/"+url_id
-        # print(f"\nURL of product: {url_full} \n")
-        list_price = product["listPrice"]["raw"]["amount"]
-        sales_price = product["salesPrice"]["raw"]["amount"]
+            # Check location if product is in one of the local stores
+            loc = product.get("location", "no location")
+            if loc != "no location":
+                location_ID = product["location"]["inventoryLocationId"]
+                hub = product["location"]["hub"]
+                location_name = product["location"]["name"]
+            else:
+                # This susually has to do with the fact that they are shipping the product directly from producer!
+                location_ID = "none"
+                hub = "none"
+                location_name = "none"
 
-        dets = product["details"]
-        design_info = product["designInformation"]
-        sales_type = product["type"]
-        title = product["title"]
+            url_full = (
+                "https://www.bolia.com/nb-no/mot-oss/butikker/online-outlet/produkt/"
+                + url_id
+            )
+            # print(f"\nURL of product: {url_full} \n")
+            list_price = product["listPrice"]["raw"]["amount"]
+            sales_price = product["salesPrice"]["raw"]["amount"]
 
-        recID = product["recId"] # Maybe this is the "unique ID that should be used? "
+            dets = product["details"]
+            design_info = product["designInformation"]
+            sales_type = product["type"]
+            title = product["title"]
 
-        # Add to products
-        products[url_id] = {
-            "title" : title,
-            "sales_type" : sales_type,
-            "design_info" : design_info,
-            "sales_price" : sales_price,
-            "list_price" : list_price,
-            "discount" : discount_percentage,
-            "url" : url_full,
-            "location_name" : location_name,
-            "location_ID" : location_ID,
-            "hub": hub
-        }
+            recID = product[
+                "recId"
+            ]  # Maybe this is the "unique ID that should be used? "
+
+            # Add to products
+            products[url_id] = {
+                "title": title,
+                "sales_type": sales_type,
+                "design_info": design_info,
+                "sales_price": sales_price,
+                "list_price": list_price,
+                "discount": discount_percentage,
+                "url": url_full,
+                "location_name": location_name,
+                "location_ID": location_ID,
+                "hub": hub,
+            }
+
+print(f"Number of products scraped: {len(products)}")
 
 # print(json.dumps(products, indent = 3))
 
